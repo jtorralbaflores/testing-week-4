0c80749f905242a72f0c72cf001d5ef77379f6a9
Javier Torralba <jtorralbaflores@gmail.com>
2026-09-11 16:27:44 +0200

    Add ivdeo features with mutate rank and distinct

--- src/data_engineering.R
+++ src/data_engineering.R
@@ -1,9 +1,24 @@
 # Load the data
+library(tidyverse)
 
+video_view <- read_csv("data/video_view.csv")
+user_view <- read_csv("data/user_view.csv")
+videos <- read_csv("data/videos.csv")
+creators <- read_csv("data/creators.csv")
+users <- read_csv("data/users.csv")
+impressions <- read_csv("data/impressions.csv")
+watch_events <- read_csv("data/watch_events.csv")
+sessions <- read_csv("data/sessions.csv")
 
 # Exercise 1
+video_features <- video_view %>%
+  mutate(watch_rate_rank = rank(-watch_rate), 
+         high_quality = avg_watch_share >= 0.40) %>%
+  distinct(video_id, .keep_all = TRUE) %>%
+  arrange(watch_rate_rank)
 
 
+write_csv(video_features, "../temp/video_features")
 
 # Exercise 2
 
