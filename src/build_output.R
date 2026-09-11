51b836bf3b68edfd475985ab1a0d0c510e4b2d6c
Javier Torralba <jtorralbaflores@gmail.com>
2026-09-11 16:54:46 +0200

    Add builds for scripts for temp and output tables

--- src/build_output.R
+++ src/build_output.R
@@ -3,3 +3,13 @@
 library(tidyverse)
 
 dir.create("output", showWarnings = FALSE)
+
+# PArt 2
+
+video_view <- read_csv("data/video_view.csv")
+
+aggregation <- video_view %>%
+  group_by(creator_id) %>%
+  summarise(sum_answer = sum(impressions_n))
+
+write_csv(aggregation, "output/creator_week4.csv")
